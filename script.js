3749b548d08644260eed8b1938c8093f2e99a9c0
henryell <henrydell4793@gmail.com>
2021-08-03 20:23:04 -0500

    First Commit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then( function(json) {
-
+            const div = document.getElementById("weather-conditions");
+            // Add HTML that includes the JSON data
 
  });
 
