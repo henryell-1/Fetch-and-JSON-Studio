7e8b9a59b899b93364a18d528093dcb61d9655e8
Henry Ell <henrydell4793@gmail.com>
2021-08-09 16:28:18 -0500

    First and last commit

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
-        response.json().then( function(json) {
-            const div = document.getElementById("weather-conditions");
-            // Add HTML that includes the JSON data
-
- });
+        return response.json();
+}).then(function(jason){
+    console.log(jason);
+    const container = document.querySelector('#container');
+    let astronauts ='';
+    for(astronaut of jason){
+        astronauts += `<div class="astronaut">
+        <div class="bio">
+           <h3>Mae Jemison</h3>
+           <ul>
+              <li>Hours in space: 190</li>
+              <li>Active: false</li>
+              <li>Skills: Physician, Chemical Engineer</li>
+           </ul>
+        </div>
+        <img class="avatar" src="images/mae-jemison.jpg">
+     </div>
+     `;
+    }
+    container.innerHTML - astronauts;
+})    
 
 });
